e4f44e2c3d9754581dbaf04a91edd3c4a81af0cb
Raiffa <raiffas@gmail.com>
2020-12-26 01:07:24 -0500

    linked keys and mp3

--- spongebob.py
+++ spongebob.py
@@ -6,25 +6,24 @@ def main():
         key = getkey()
         if key == keys.Q:
             playsound('resources/meanwhile_sound_effect.mp3')
-            print(keys.Q)
         elif key == keys.W:
-            print(keys.W)
+            playsound('resources/A_Few_Moments_Later_SpongeBob_Time_Card_8.mp3')
         elif key == keys.E:
-            print(keys.E)
+            playsound('resources/Patrick_Booing_Sound_Effect__Download.mp3')
         elif key == keys.R:
-            print(keys.R)
+            playsound('resources/SpongeBob_-_Squidward_youre_back_Sound_effect__Download.mp3')
         elif key == keys.T:
-            print(keys.T)
+            playsound('resources/Spongebob_-_Alright_pinhead_Sound_Effect__Download.mp3')
         elif key == keys.Y:
-            print(keys.Y)
+            playsound('resources/Spongebob_-_Okay_Im_ready_Sound_Effect__Download.mp3')
         elif key == keys.U:
-            print(keys.U)
+            playsound('resources/Spongebob_-_Overtime_Sound_effect__Download.mp3')
         elif key == keys.I:
-            print(keys.I)
+            playsound('resources/Spongebob_-_Weird_Gibberish_Sound_Effect__Download.mp3')
         elif key == keys.O:
-            print(keys.O)
+            playsound('resources/Squidward_-_I_have_no_talent_Sound_Effect__Download.mp3')
         elif key == keys.P:
-            print(keys.P)
+            playsound('resources/Squidward_-_Shut_up_Slapping_Sound_Effect__Download.mp3')
         elif key == keys.ESC:
             return
         else:
